4c378eabeb5ae751d692014d49f35cc31df99f31
BettyYHChen <betty_c@outlook.com>
2019-04-25 12:31:53 -0400

    remove the popout window

---
+++ src/main/java/com/busyqa/crm/config/AuthenticationFilter.java
@@ -0,0 +1,53 @@
+package com.busyqa.crm.config;
+
+import com.busyqa.crm.auth.SecurityConstant;
+import com.busyqa.crm.auth.UserLoginRequestModel;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+
+
+
+public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    private final AuthenticationManager authenticationManager;
+
+    public AuthenticationFilter(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest req,
+                                                HttpServletResponse res) throws AuthenticationException {
+        try {
+
+
+            UserLoginRequestModel creds = new ObjectMapper()
+                    .readValue(req.getInputStream(), UserLoginRequestModel.class);
+
+            return authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            creds.getUserName(),
+                            creds.getPassword(),
+                            new ArrayList<>())
+            );
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+
+}
